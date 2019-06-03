76042bbb3be96ea71ba07c13fd72d1c02af68d48
Drashti Gadhia <drashtibgadhia10@gmail.com>
2019-06-03 14:42:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { connect } from 'react-redux'
+import { sendMessage } from './actions'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    message: ''
+  }
+
+  onChange = (event) => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
+  onSend = (event) => {
+    event.preventDefault()
+
+    console.log('onSend test!')
+    this.props.sendMessage(this.state.message)
+
+    this.setState({ message: '' })
+  }
+
+  render () {
+    const paragraphs = this
+      .props
+      .messages
+      .map((message, index) => <p key={index}>{message}</p>)
+
+    return (
+      <main>
+        <h1>Messages:</h1>
+
+        {paragraphs}
+
+        <form onSubmit={this.onSend}>
+          <input
+            type='text'
+            onChange={this.onChange}
+            value={this.state.message}
+          />
+          <button>send</button>
+          { this.props.sent && 'checkmark' }
+        </form>
+      </main>
+    )
+  }
 }
 
-export default App;
+function mapStateToProps (state) {
+  console.log('state test:', state)
+  return {
+    messages: state.messages,
+    sent: state.sent
+  }
+}
+
+const mapDispatchToProps = {
+  sendMessage
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
